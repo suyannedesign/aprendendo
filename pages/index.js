f68b78a41e621f546377182ff4482f5d792b078e
suyannedesign <suyannemmendes@gmail.com>
2026-03-03 12:46:43 +0000

    adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Oi, Rafa! Obrigada por me emprestar seu curso do Filipe D. (não sei escrever o sobrenome dele heheh) e por me ajudar tirando minhas dúvidas :) te amo! sz</h1>
+}
+
+export default Home;
